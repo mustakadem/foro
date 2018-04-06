dd10a7298f24792e407d5f044a24611350923f56
musta <mustakadem@gmail.com>
2018-04-06 12:03:38 +0200

    se añade la autenticacion del usuario y el registro

--- probandoDjango/settings.py
+++ probandoDjango/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards'
+    'widget_tweaks',
+    'boards',
+    'accounts'
 ]
 
 MIDDLEWARE = [
