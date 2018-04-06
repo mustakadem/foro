12ba8507c9a43b04d012c35fc4829200f965b95d
musta <mustakadem@gmail.com>
2018-04-06 13:44:16 +0200

    se separan los test dependiendo de su funcion para estructurar menor el codigo

--- accounts/test/test_view_signup.py
+++ accounts/test/test_view_signup.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from ..forms import SignUpForm
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
-from .views import signup
+from ..views import signup
 
 
 class SignUpTest(TestCase):
@@ -23,7 +23,17 @@ class SignUpTest(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        """
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SuccessfulSignUpTests(TestCase):
